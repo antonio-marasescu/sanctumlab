76a21c3dd7c50b005167140d15e269fcb0937c8e
antonio <antonio.marasescuduran@gmail.com>
2024-10-30 23:20:54 +0200

    chore: remove unused test

--- apps/sanctumlab-api/src/lib/sanctumlab-api.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sanctumlabApi } from './sanctumlab-api';
-
-describe('sanctumlabApi', () => {
-    it('should work', () => {
-        expect(sanctumlabApi()).toEqual('sanctumlab-api');
-    });
-});
