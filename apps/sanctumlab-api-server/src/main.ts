d30a6610d09f133ddc5670c67dc66091b88357d1
antoniomarasescuduran <antonio.marasescu-duran@msg.group>
2024-10-31 16:51:37 +0200

    feat: added be implementation

--- apps/sanctumlab-api-server/src/main.ts
+++ apps/sanctumlab-api-server/src/main.ts
@@ -1,18 +1,13 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
 import * as path from 'path';
+import productsRouter from './middlewares/routes/products.router';
 
 const app = express();
+const baseRoute = 'api';
 
+app.use(express.json());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
-    res.send({ message: 'Welcome to sanctumlab-api-server!' });
-});
+app.use(`${baseRoute}/products`, productsRouter);
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
---
+++ apps/sanctumlab-api-server/src/middlewares/routes/products.router.ts
@@ -0,0 +1,54 @@
+import express from 'express';
+import { ProductsApiInstance } from '@sanctumlab/be/products-feature';
+
+const productsRouter = express.Router();
+
+productsRouter.post('', async (req, res) => {
+    try {
+        const newProduct = await ProductsApiInstance.create(req.body);
+        res.status(201).json(newProduct);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+productsRouter.get('', async (req, res) => {
+    try {
+        const products = await ProductsApiInstance.retrieveAll();
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+productsRouter.get(`/:id`, async (req, res) => {
+    try {
+        const product = await ProductsApiInstance.retrieveById(req.params.id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+productsRouter.put(`/:id`, async (req, res) => {
+    try {
+        const updatedProduct = await ProductsApiInstance.update(
+            req.params.id,
+            req.body
+        );
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+productsRouter.delete(`/:id`, async (req, res) => {
+    try {
+        const isRemoved = await ProductsApiInstance.removeById(req.params.id);
+        res.status(200).json({ ok: isRemoved });
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+export default productsRouter;
