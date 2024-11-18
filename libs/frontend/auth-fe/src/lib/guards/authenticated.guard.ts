47cef8d244cc97597437e0e3ef488d0eb01cb41b
antonio <antonio.marasescuduran@gmail.com>
2024-11-18 23:16:12 +0200

    fix: auth issue

--- libs/frontend/auth-fe/src/lib/guards/authenticated.guard.ts
+++ libs/frontend/auth-fe/src/lib/guards/authenticated.guard.ts
@@ -1,16 +1,20 @@
 import { inject } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthFeatureName, AuthRoutes } from '../types/auth-navigation.types';
 
 export const authenticatedGuard = async (): Promise<boolean> => {
     const authService = inject(AuthenticationService);
+    const route = inject(ActivatedRoute);
     const router = inject(Router);
 
+    const queryParams = route.snapshot.queryParams;
     const isAuthenticated = await authService.isAuthenticated();
 
     if (!isAuthenticated) {
-        await router.navigate([AuthFeatureName, AuthRoutes.LOGIN]);
+        await router.navigate([AuthFeatureName, AuthRoutes.LOGIN], {
+            queryParams
+        });
         return true;
     }
 
